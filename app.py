baa415cf29d907258c02e0fd45a384d9484e1e9d
Gurraj Bains <bainsgurraj13@gmail.com>
2026-02-09 21:17:13 -0800

    Fixed data not syncing correctly with columns on webpage

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-from database import init_db, add_transaction, get_all_transactions
+from database import init_db, add_transaction, get_all_transactions, return_HTML_table
 
 appp = Flask(__name__)
 
 
 @appp.route('/')
 def house():
-    return render_template('index.html')
+    rows = get_all_transactions()
+    return render_template('index.html', rows=rows)
 if(__name__ == '__main__'):
     init_db()
     appp.run(debug=True)
+
+    #v \Scripts\activate venv  to activate virtual environment
