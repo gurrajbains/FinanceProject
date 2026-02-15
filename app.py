0c0673090e242a30b290616a669e65120aebd4d3
Gurraj Bains <bainsgurraj13@gmail.com>
2026-02-15 14:06:13 -0800

    Changed method of getting data as well as fixed get_summary logic that resulted in errors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, app, render_template, request, redirect, send_file, url_for
-from database import delete_transaction, init_db, add_transaction, get_all_transactions, return_HTML_table, delete_all_transactions, export_to_csv, get_summary, get_transactions_by_type, search_transactions
+from cProfile import label
+from flask import Flask, app, jsonify, render_template, request, redirect, send_file, url_for
+from database import delete_transaction, init_db, add_transaction, get_all_transactions, return_HTML_table, delete_all_transactions, export_to_csv, get_summary, search_transactions
 
 appp = Flask(__name__)
 
@@ -13,7 +14,7 @@ def add():
     name = request.form["name"].strip()
     date = request.form["date"].strip()
     amount = float(request.form["amount"])
-    ttype = "income"
+    ttype = request.form["type"].strip()
     category = request.form["category"].strip()
     description = request.form.get("description", "").strip()
 
@@ -42,6 +43,15 @@ def search():
     rows = search_transactions(q, ttype)
     return render_template("index.html", rows=rows, q=q, type=ttype)
 
+@appp.route("/api/summary", methods=["GET"])
+def api_summary():
+    summary_data = get_summary()
+    labels = []
+    values = []
+    for item in summary_data:
+        labels.append(item[0])
+        values.append(item[1])
+    return  jsonify({"labels": labels, "values": values})
 
 if(__name__ == '__main__'):
     init_db()
--- testconnection.py
+++
@@ -1,9 +0,0 @@
-from database import get_connection
-
-def main():
-    conn = get_connection()
-    conn.close()
-    print("Connection OK")
-
-if __name__ == "__main__":
-    main()
